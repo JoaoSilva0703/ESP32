1e9731c70d14ef26b91f9d8c7707952bf45a5706
João Silva <JoaoSilva0703@protonmail.com>
2026-03-23 23:00:27 +0000

    Uart Demo - echo working

--- myCode/demo/src/serialDemo.c
+++ myCode/demo/src/serialDemo.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <driver/uart.h>
+#include <hal/uart_hal.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define UART_DRIVER_BUFF_SIZE (256)
+uint8_t uart_buffer[UART_DRIVER_BUFF_SIZE];
 void DEMO_job(void *arg)
 {   
     (void)arg; // Avoid unused parameter warning
-
-    while(1) 
+    uint32_t bytes_read = 0;
+    if(uart_driver_install(UART_NUM_0, UART_DRIVER_BUFF_SIZE, UART_DRIVER_BUFF_SIZE, 0, NULL, 0) == ESP_OK)
     {
-        uart_write_bytes(UART_NUM_0, "Hello João\n", 13);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    
+        while(1) 
+        {
+            bytes_read = uart_read_bytes(UART_NUM_0, uart_buffer, UART_DRIVER_BUFF_SIZE, 100 / portTICK_PERIOD_MS);
+            if(bytes_read > 0) // Clear the RX buffer
+            {
+                uart_write_bytes(UART_NUM_0, uart_buffer, bytes_read);
+            }
+        }
+    }    
 }
