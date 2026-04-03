2f71609b01d551ec565ce56d275c3fec65e06bde
João Silva <JoaoSilva0703@protonmail.com>
2026-04-03 22:07:14 +0100

    Update DEMO to use new UART code

--- myCode/demo/inc/serialDemo.h
+++ myCode/demo/inc/serialDemo.h
@@ -1,1 +1,8 @@
 void DEMO_job(void *arg);
+void uartEventTask(void *pvParameters);
+// typedef struct
+// {
+//     DEMO_options_t *childsNodes;
+//     DEMO_options_t *parentNode;
+//     void (*job)(void *arg);
+// } DEMO_options_t;
--- myCode/demo/src/serialDemo.c
+++ myCode/demo/src/serialDemo.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include <driver/uart.h>
-#include <hal/uart_hal.h>
+#include "uart.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define UART_DRIVER_BUFF_SIZE (256)
-uint8_t uart_buffer[UART_DRIVER_BUFF_SIZE];
+#define DEMO_MESSAGE_SIZE (256)
+uint8_t demoBuffer[DEMO_MESSAGE_SIZE];
+
 void DEMO_job(void *arg)
 {   
     (void)arg; // Avoid unused parameter warning
-    uint32_t bytes_read = 0;
-    if(uart_driver_install(UART_NUM_0, UART_DRIVER_BUFF_SIZE, UART_DRIVER_BUFF_SIZE, 0, NULL, 0) == ESP_OK)
+    uint32_t length = 0;
+    while(1)
     {
-        while(1) 
+        UART_read(demoBuffer, &length);
+
+        if(length > 0)
         {
-            bytes_read = uart_read_bytes(UART_NUM_0, uart_buffer, UART_DRIVER_BUFF_SIZE, 100 / portTICK_PERIOD_MS);
-            if(bytes_read > 0) // Clear the RX buffer
-            {
-                uart_write_bytes(UART_NUM_0, uart_buffer, bytes_read);
-            }
+            UART_write((const char *)demoBuffer, length);
         }
-    }    
+        vTaskDelay(pdMS_TO_TICKS(1)); // Delay for 1 second
+    }
 }
